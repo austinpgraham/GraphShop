9bc8fb11bd78d5e9d2fe22beeee2904e68201e10
Austin Graham <austingraham731@gmail.com>
2018-10-21 00:13:49 -0500

    product get route

--- src/gs/views/product.py
+++ src/gs/views/product.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
+import json
+
+from flask import Response
 from flask import Blueprint
 
+from gs.dataserver.model.product import get_products
 
 PRODUCTS_ROUTE = Blueprint("products", __name__)
 
 
 @PRODUCTS_ROUTE.route('/<pid>', methods=['GET'])
 def get_product(pid):
-    return ""
+    pids = set(pid.split('&')) if '&' in pid else [pid]
+    products = [p.__dict__ for p in get_products(pids)]
+    for p in products:
+        p.pop('_sa_instance_state')
+    return Response(json.dumps(products, skipkeys=True), mimetype="application/json")
 
 
 def register_products(app):
--- src/gs/views/tests/test_product.py
+++ src/gs/views/tests/test_product.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+from hamcrest import is_
+from hamcrest import has_length
+from hamcrest import assert_that
+
 from gs.views.tests import WebTest
 
 from gs.views.product import *
@@ -8,4 +12,10 @@ class TestProducts(WebTest):
 
     def test_product_get(self):
         result = self.app.get('/products/123')
-        from pdb import set_trace; set_trace()
+        assert_that(result.json, is_([]))
+
+        result = self.app.get('/products/0123456748')
+        assert_that(result.json.pop()['asin'], is_('0123456748'))
+
+        result = self.app.get('/products/0123456748&0123456788')
+        assert_that(result.json, has_length(2))
