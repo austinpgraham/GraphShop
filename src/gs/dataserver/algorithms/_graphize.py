01d409845750c960ebb7996d172cf9568c82eda7
Austin Graham <austingraham731@gmail.com>
2018-11-25 14:24:33 -0600

    done

--- src/gs/dataserver/algorithms/_graphize.py
+++ src/gs/dataserver/algorithms/_graphize.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 from gs.dataserver.model.product import get_products
 from gs.dataserver.model.product import product_exists
 
 
-_PRODUCT_LIMIT = 200
+_PRODUCT_LIMIT = 30
 
 
 class _Vertex():
@@ -24,7 +27,7 @@ class _Vertex():
                 'onclick': "clickedNode(this)"
             }
         }
-        return json.dumps(data)
+        return data
 
 class _Edge():
 
@@ -35,13 +38,13 @@ class _Edge():
     def to_json(self):
         data = {
             'source': self.source,
-            'dest': self.dest
+            'dest': self.dest,
             'vis': {
                 'stroke': '#ddd',
                 'stroke_width': '2px'
             }
         }
-        return json.dumps(data)
+        return data
 
 
 class ProductGraph():
@@ -51,27 +54,29 @@ class ProductGraph():
             raise ValueError("Product {} does not exist.".format(start_asin))
         self.products = [start_asin]
         self.categories = []
-        self._breadth_first(start_asin)
         self.vertices = []
         self.edges = []
+        self._breadth_first(start_asin)
     
     def _breadth_first(self, node):
         to_visit = [node]
-        while to_visit and len(self.products) < _PRODUCT_LIMIT:
+        while to_visit:
             _next = to_visit.pop()
             product = get_products([_next])[0]
-            cats = product.categories[0]
+            cats = product.categories[0] if len(product.categories) > 0 else []
             for c in cats:
                 if c not in self.categories:
                     self.categories.append(c)
             cat_idx = self.categories.index(cats[0]) if len(cats) > 0 else -1
             p_idx = self.products.index(_next)
             self.vertices.append(_Vertex(p_idx, cat_idx))
-            for p in product.related['also_bought']:
-                if p not in self.products and product_exists(p):
-                    to_visit.append(p)
-                    self.products.append(p)
-                    self.edges.append(_Edge(p_idx, self.products[-1]))
+            if 'also_bought' in product.related and len(self.products) < _PRODUCT_LIMIT:
+                for p in product.related['also_bought']:
+                    if p not in self.products and product_exists(p):
+                        to_visit.append(p)
+                        self.products.append(p)
+                        logging.info('Current product count: {}.'.format(len(self.products)))
+                        self.edges.append(_Edge(p_idx, self.products.index(p)))
 
     def to_json(self):
         data = {
---
+++ src/gs/dataserver/scripts/graph.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import json
+
+from gs.dataserver.algorithms import ProductGraph
+
+START_ASIN = '048640871X'
+
+def main():
+    graph = ProductGraph(START_ASIN)
+    json.dump(graph.to_json(), open('graph.json', 'w'))
+
+if __name__ == '__main__':
+    main()
